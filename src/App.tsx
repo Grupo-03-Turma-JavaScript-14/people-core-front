fb43ee2265a658615b3fbe66a95d33db713185db
Kauvinzera <kauvini2007@gmail.com>
2026-05-22 15:39:16 -0300

    feat: departamentos ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Departamento from './pages/Departamentos/Departamentos'
 
@@ -10,16 +9,9 @@ function App() {
       <Routes>
         <Route path="/departamentos" element={<Departamento />}></Route>
       </Routes>
-      
       </BrowserRouter>
     </>
   )
-=======
-import AppRoutes from "./Pages/AppRoutes";
-
-function App() {
-  return <AppRoutes />;
->>>>>>> 1ad60b0b688ec2d870c83774a624cee648ebca5f
 }
 
 export default App;
