74a8902a5027dcad4b7fe7b030aa4be248a688d5
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-05-24 12:53:03 -0300

    feat: rebase main/home

--- src/Pages/AppRoutes.tsx
+++ src/Pages/AppRoutes.tsx
@@ -1,13 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 
+import Home from "./Home/Home";
 import Testes from "./Testes/Testes";
 
 function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<Testes />} />
+      <Route path="/" element={<Home />} />
       <Route path="/testes" element={<Testes />} />
-      <Route path="*" element={<Testes />} />
+      <Route path="*" element={<Home />} />
     </Routes>
   );
 }
