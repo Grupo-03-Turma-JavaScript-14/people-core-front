1556b6794293dcb74bd03adf3d83059375e944db
Kefilwe Lourenco <kefilwebr@hotmail.com>
2026-06-15 16:50:04 -0300

    fix: remove marcadores de conflito do Sobre

--- src/Pages/Sobre/Sobre.tsx
+++ src/Pages/Sobre/Sobre.tsx
@@ -1,7 +1,6 @@
 import { motion, type Variants } from "framer-motion";
 import "../../Style/Css/Pages/Sobre.css";
 import "../../Style/Css/Components/About/TeamPeopleCore.css"; 
-import "../../Style/Css/components/About/TeamPeopleCore.css";
 import TeamPeopleCore from "../../Components/About/TeamPeopleCore";
 
 const equipe = [
